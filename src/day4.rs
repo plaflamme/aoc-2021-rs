42464f5ad5e595b2815ef56db1a0783cc32467c1
plaflamme <philippe@hopper.com>
2021-12-04 00:17:35 -0500

    day 4 parser

--- src/day4.rs
+++ src/day4.rs
@@ -1,9 +1,34 @@
-pub struct Solution;
+#[derive(Clone, Debug)]
+struct Board {
+    cells: Vec<Vec<u32>>,
+}
+
+pub struct Solution(Vec<u32>, Vec<Board>);
 
 impl super::Solver for Solution {
-    const SAMPLE: &'static str = "";
+    const SAMPLE: &'static str =
+        "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
-    const LEVEL1: &'static str = "";
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+";
+
+    const LEVEL1: &'static str = "4512";
 
     const LEVEL2: &'static str = "";
 
@@ -13,7 +38,33 @@ impl super::Solver for Solution {
     where
         Self: Sized,
     {
-        todo!()
+        let mut lines = input.lines();
+        let nums = lines.next().unwrap();
+        let nums = nums.split(',').map(|n| n.parse::<u32>().unwrap()).collect();
+
+        lines.next();
+        let (mut boards, last_board) = lines.fold(
+            (Vec::new(), Vec::new()),
+            |(mut boards, mut cells), line| match line {
+                "" => {
+                    boards.push(Board { cells });
+                    (boards, Vec::new())
+                }
+                nums => {
+                    let row = nums
+                        .split(' ')
+                        .filter(|n| n.len() > 0)
+                        .map(|n| n.parse::<u32>().unwrap())
+                        .collect();
+                    cells.push(row);
+                    (boards, cells)
+                }
+            },
+        );
+
+        boards.push(Board { cells: last_board });
+
+        Solution(nums, boards)
     }
 
     fn part1(self) -> Self::Output {
