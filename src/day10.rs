9fc1f3c715c4c6bad79b9baf2c4a59f0603ea80f
plaflamme <philippe@hopper.com>
2021-12-10 00:21:05 -0500

    day 10 part 1

--- src/day10.rs
+++ src/day10.rs
@@ -1,21 +1,80 @@
 use crate::{Day10, Solver};
 
-sample!(Day10, "", "");
+sample!(
+    Day10,
+    "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]",
+    "26397"
+);
+
+fn first_illegal_char(s: &str) -> Option<char> {
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        log::debug!("char is {}", c);
+        match c {
+            '[' | '(' | '{' | '<' => stack.push(c),
+            ']' | ')' | '}' | '>' => match stack.pop() {
+                None => return Some(c),
+
+                Some(opening) => match (opening, c) {
+                    ('[', ']') | ('(', ')') | ('{', '}') | ('<', '>') => (),
+                    _ => {
+                        return Some(c);
+                    }
+                },
+            },
+            _ => panic!("illegal char {}", c),
+        }
+    }
+    None
+}
 
 impl Solver for Day10 {
     type Output = u32;
 
-    type Input = String;
+    type Input = Vec<String>;
 
     fn parse(input: &str) -> Self::Input {
-        todo!()
+        input.lines().map(|s| s.to_string()).collect()
     }
 
     fn part1(input: Self::Input) -> Self::Output {
-        todo!()
+        input
+            .into_iter()
+            .flat_map(|l| first_illegal_char(&l))
+            .map(|c| match c {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+                _ => panic!("invalid illegal char {}", c),
+            })
+            .sum::<u32>()
     }
 
     fn part2(input: Self::Input) -> Self::Output {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_something() {
+        let c = '(';
+        let closing = ')';
+
+        match (c, closing) {
+            ('(', ')') => (),
+            _ => panic!("damn"),
+        }
     }
 }
